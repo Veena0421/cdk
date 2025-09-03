17169fa6377ea41891198d6dda60260ea471ddf1
Veena0421 <tmadhuri8589@gmail.com>
2025-09-04 02:28:35 +0530

    Fix nested repository and add all project files

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World from an ECS Fargate Container! 🚀"
+
+if __name__ == '__main__':
+    # Listens on all public IPs.
+    # The port is specified by the Dockerfile.
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
