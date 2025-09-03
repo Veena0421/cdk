73534732ebd43868533df7341accf27db077058d
Veena0421 <tmadhuri8589@gmail.com>
2025-09-04 03:07:08 +0530

    Checking the deployment

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World from an ECS Fargate Container! another check 🚀"
+    return "Checking for auto deployment"
 
 if __name__ == '__main__':
     # Listens on all public IPs.
